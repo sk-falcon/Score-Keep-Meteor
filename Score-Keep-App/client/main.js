692773177dce8aaa0845bc215fac8a897dfb216d
sk-falcon <sparsh.dtu@gmail.com>
2017-08-18 20:42:03 +0530

    - Input Form Added

--- Score-Keep-App/client/main.js
+++ Score-Keep-App/client/main.js
@@ -1,29 +1,9 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import {Meteor} from 'meteor/meteor'
+import {Tracker} from 'meteor/tracker'
 
-const players = [
-{
-  _id: '1',
-  name: 'A',
-  score: 100
-},
-{
-  _id: '2',
-  name: 'B',
-  score: 200
-},
-{
-  _id: '3',
-  name: 'C',
-  score: 300
-},
-{
-  _id: '4',
-  name: 'D',
-  score: 400
-}
-];
+import {Players} from './../imports/api/players'
 
 const renderPlayers = function(playersList){
   return playersList.map(function(obj) {
@@ -31,18 +11,44 @@ const renderPlayers = function(playersList){
   });
 };
 
+const handleSubmit = function(e){
+  let playerName = e.target.playerName.value;
 
+  e.preventDefault();
+
+  if(playerName){
+    e.target.playerName.value = '';
+
+    Players.insert({
+      name: playerName,
+      score: 99
+    });
+  }
+
+};
 
 Meteor.startup(function(){
-  let title = 'Sparsh';
-  let name = 'SK';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      <p>Hello {name}!</p>
-      <p>This is the Second Para</p>
-      {renderPlayers(players)}
-    </div>
-  );
-  ReactDom.render(jsx, document.getElementById('app'));
+
+  Tracker.autorun( function() {
+
+    let players = Players.find().fetch();
+    let title = 'Sparsh';
+    let name = 'SK';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is the Second Para</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player Name"/>
+          <button>Add Player</button>
+        </form>
+      </div>
+    );
+    ReactDom.render(jsx, document.getElementById('app'));
+
+  });
+
+
 });
--- Score-Keep-App/server/main.js
+++ Score-Keep-App/server/main.js
@@ -2,10 +2,10 @@ import {Meteor} from 'meteor/meteor'
 import {Players} from './../imports/api/players'
 
 Meteor.startup(function(){
-  Players.insert({
-    name: 'Player 3',
+  /*Players.insert({
+    name: 'Player 4',
     score: 100
-  })
+  });*/
 
   console.log(Players.find().fetch());
 

bb03720068fd831e17be8ccb580748be77c6374f
sk-falcon <sparsh.dtu@gmail.com>
2017-08-21 21:25:35 +0530

    - Sorting implemented

--- Score-Keep-App/client/main.js
+++ Score-Keep-App/client/main.js
@@ -4,27 +4,18 @@ import {Meteor} from 'meteor/meteor'
 import {Tracker} from 'meteor/tracker'
 
 import {Players} from './../imports/api/players'
-import TitleBar from './../imports/ui/TitleBar'
-import AddPlayer from './../imports/ui/AddPlayer'
-import Player from './../imports/ui/Player'
-import PlayerList from './../imports/ui/PlayerList'
+
+import App from './../imports/ui/App'
 
 Meteor.startup(() => {
 
   Tracker.autorun( () => {
 
-    let players = Players.find().fetch();
+    let players = Players.find({},{sort: {score:-1}}).fetch();
     let title = 'Score Keep App'
-    let subtitle = 'created by SK'
+    // let subtitle = 'created by SK'
 
-    let jsx = (
-      <div>
-        <TitleBar title={title} subtitle={subtitle}/>        
-        <PlayerList players={players}/>
-        <AddPlayer/>
-      </div>
-    );
-    ReactDom.render(jsx, document.getElementById('app'));
+    ReactDom.render(<App title={title} players={players}/>, document.getElementById('app'));
 
   });
 
--- Score-Keep-App/imports/ui/PlayerList.js
+++ Score-Keep-App/imports/ui/PlayerList.js
@@ -3,10 +3,14 @@ import Player from './Player'
 
 export default class PlayerList extends React.Component{
   renderPlayers () {
-    return this.props.players.map((player) => {
-      return <Player key = {player._id} player = {player}/>;
 
-    });
+    if(this.props.players.length === 0) {
+      return <p>Add a new player to get started</p>
+
+    } else {
+      return this.props.players.map((player) => {
+      return <Player key = {player._id} player = {player}/>;
+    });}
   }
 
   render(){

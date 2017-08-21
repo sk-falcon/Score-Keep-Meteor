81d1f728e8e64b6c1a96f5560cf859ee4a3435f5
sk-falcon <sparsh.dtu@gmail.com>
2017-08-21 15:08:50 +0530

    - React List Added

--- Score-Keep-App/client/main.js
+++ Score-Keep-App/client/main.js
@@ -6,50 +6,21 @@ import {Tracker} from 'meteor/tracker'
 import {Players} from './../imports/api/players'
 import TitleBar from './../imports/ui/TitleBar'
 import AddPlayer from './../imports/ui/AddPlayer'
-
-const renderPlayers = (playersList) => {
-  return playersList.map((obj) => {
-    return (
-      <p key={obj._id}>
-        {obj.name} has {obj.score} point(s).
-        <button onClick={() => Players.update({_id: obj._id},{$inc:{score: 1} })}>+1</button>
-        <button onClick={() => Players.update({_id: obj._id},{$inc:{score: -1} })}>-1</button>
-        <button onClick={() => Players.remove({_id: obj._id})}>X</button>
-      </p>
-    );
-  });
-};
-
-const handleSubmit = (e) => {
-  let playerName = e.target.playerName.value;
-
-  e.preventDefault();
-
-  if(playerName){
-    e.target.playerName.value = '';
-
-    Players.insert({
-      name: playerName,
-      score: 0
-    });
-  }
-
-};
+import Player from './../imports/ui/Player'
+import PlayerList from './../imports/ui/PlayerList'
 
 Meteor.startup(() => {
 
   Tracker.autorun( () => {
 
     let players = Players.find().fetch();
+    let title = 'Score Keep App'
+    let subtitle = 'created by SK'
 
     let jsx = (
       <div>
-        <TitleBar/>
-        {renderPlayers(players)}
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="playerName" placeholder="Player Name"/>
-          <button>Add Player</button>
-        </form>
+        <TitleBar title={title} subtitle={subtitle}/>        
+        <PlayerList players={players}/>
         <AddPlayer/>
       </div>
     );
--- Score-Keep-App/imports/ui/TitleBar.js
+++ Score-Keep-App/imports/ui/TitleBar.js
@@ -1,11 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 export default class  TitleBar extends React.Component {
   render(){
     return (
       <div>
-        <h1>Score Keep</h1>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
 }
+
+TitleBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired
+};
+
+TitleBar.defaultProps = {
+  // title: 'Default Title'
+};

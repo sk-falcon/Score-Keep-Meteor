6a3699e8fe5d48cddf248ab49596ef7cd56f7b0a
sk-falcon <sparsh.dtu@gmail.com>
2017-08-25 13:46:44 +0530

    - v1 finished

--- Score-Keep-App/imports/ui/AddPlayer.js
+++ Score-Keep-App/imports/ui/AddPlayer.js
@@ -8,7 +8,10 @@ export default class AddPlayer extends React.Component{
 
     e.preventDefault();
 
+    debugger;
+    
     if(playerName){
+
       e.target.playerName.value = '';
 
       Players.insert({
